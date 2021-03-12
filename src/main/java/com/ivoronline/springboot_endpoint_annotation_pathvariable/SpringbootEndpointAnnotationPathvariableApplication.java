8fe192159f7d034434d28983c3f99ae15f000b94
igoreta <ivoronline@gmail.com>
2021-03-12 14:43:51 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_endpoint_annotation_pathvariable/SpringbootEndpointAnnotationPathvariableApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_endpoint_annotation_pathvariable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEndpointAnnotationPathvariableApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootEndpointAnnotationPathvariableApplication.class, args);
+  }
+
+}
